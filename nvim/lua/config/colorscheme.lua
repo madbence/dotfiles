d5ad8fc9e02507886f5807973382ec149629f16b
Bence Dányi <lennon@Bences-MacBook-Pro.local>
2022-03-18 01:07:10 +0100

    feat: fix telescope colors

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,19 +1,11 @@
 local M = {}
 function M.setup()
-  vim.cmd [[colorscheme base16-ocean]]
-
-  -- by default, Telescope borders are invisible
-  -- see https://github.com/RRethy/nvim-base16/issues/32#issuecomment-1026475234
-  vim.cmd [[highlight! link TelescopeSelection    Visual]]
-  vim.cmd [[highlight! link TelescopeNormal       Normal]]
-  vim.cmd [[highlight! link TelescopePromptNormal TelescopeNormal]]
-  vim.cmd [[highlight! link TelescopeBorder       TelescopeNormal]]
-  vim.cmd [[highlight! link TelescopePromptBorder TelescopeBorder]]
-  vim.cmd [[highlight! link TelescopeTitle        TelescopeBorder]]
-  vim.cmd [[highlight! link TelescopePromptTitle  TelescopeTitle]]
-  vim.cmd [[highlight! link TelescopeResultsTitle TelescopeTitle]]
-  vim.cmd [[highlight! link TelescopePreviewTitle TelescopeTitle]]
-  vim.cmd [[highlight! link TelescopePromptPrefix Identifier]]
+  require('base16-colorscheme').with_config({
+    telescope = false,
+  })
+  local theme = vim.env.BASE16_THEME
+  if not theme then theme = 'ocean' end
+  vim.cmd('colorscheme base16-' .. theme)
 end
 
 return M
