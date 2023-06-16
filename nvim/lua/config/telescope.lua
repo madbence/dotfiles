064fcada95001bd4483c5005fda48494f2e7c4ba
Bence Dányi <bence@danyi.me>
2023-06-16 10:14:15 +0200

    feat: add more telescope bindings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -4,6 +4,12 @@ function M.setup()
   local telescope = require('telescope')
   telescope.setup({})
   telescope.load_extension('ui-select')
+
+  local builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 end
 
 return M
